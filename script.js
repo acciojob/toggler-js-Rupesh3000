966c20f522a57034f79f0b29c45e212e31ea8480
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2026-02-17 22:06:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const toggles = document.querySelectorAll(".toggle");
+
+let selected = [];
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      // Add newly checked toggle to array
+      selected.push(e.target);
+
+      // If more than 2 are selected
+      if (selected.length > 2) {
+        // Remove the first (oldest)
+        const firstSelected = selected.shift();
+        firstSelected.checked = false;
+      }
+    } else {
+      // If user manually unchecks something
+      selected = selected.filter((item) => item !== e.target);
+    }
+  });
+});
